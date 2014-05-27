4399200ac15b584f41d16937049277c627035214
Vít Kutný <vitkutny@outlook.com>
2014-05-27 02:49:35 +0200

    dev

--- src/Gallery/Extension.php
+++ src/Gallery/Extension.php
@@ -15,13 +15,28 @@ final class Extension extends WebEdit\Extension {
     ];
 
     public function loadConfiguration() {
-        parent::loadConfiguration();
         $config = $this->getConfig($this->defaults);
         $builder = $this->getContainerBuilder();
-        $builder->getDefinition($this->prefix('photo.facade'))
+        $builder->addDefinition($this->prefix('repository'))
+                ->setClass('WebEdit\Gallery\Repository');
+        $builder->addDefinition($this->prefix('facade'))
+                ->setClass('WebEdit\Gallery\Facade');
+        $builder->addDefinition($this->prefix('control'))
+                ->setImplement('WebEdit\Gallery\Control\Factory')
+                ->setParameters(['gallery'])
+                ->setArguments([new \Nette\PhpGenerator\PhpLiteral('$gallery')]); //TODO
+        $builder->addDefinition($this->prefix('photo.repository'))
+                ->setClass('WebEdit\Gallery\Photo\Repository');
+        $builder->addDefinition($this->prefix('photo.facade'))
+                ->setClass('WebEdit\Gallery\Photo\Facade')
                 ->setArguments([$config['photo']['upload']]);
-        $builder->getDefinition($this->prefix('photo.presenter'))
+        $builder->addDefinition($this->prefix('photo.presenter'))
+                ->setClass('WebEdit\Gallery\Photo\Presenter')
                 ->setArguments([$config['photo']]);
+        $builder->addDefinition($this->prefix('photo.control'))
+                ->setImplement('WebEdit\Gallery\Photo\Control\Factory')
+                ->setParameters(['photo'])
+                ->setArguments([new \Nette\PhpGenerator\PhpLiteral('$photo')]); //TODO
     }
 
 }
